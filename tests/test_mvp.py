aa4efb9cb82893ad161dcf1ad543cde14e0e81d7
Valerie Valentin <valerie.valentine93@gmail.com>
2024-12-12 00:04:25 -0800

    added console_view & tests

---
+++ tests/test_mvp.py
@@ -0,0 +1,237 @@
+import pytest
+from model.game import Game
+from model.guess import Guess
+
+
+def test_game_initialization():
+    # arrange & act
+    game = Game(4)
+
+    # assert
+    assert game.lives == 10
+    assert game.previous_guesses == []
+    assert game.game_status == "In Progress"
+
+
+def test_game_generates_answer_for_different_levels():
+    # arrange
+    levels = {"easy": 4, "medium": 6, "hard": 8}
+
+    # act
+    for level_digits in levels.values():
+        game = Game(level_digits)
+        game.generate_answer()
+
+        # assert
+        assert len(game.answer) == level_digits
+        assert isinstance(game.answer, str)
+
+
+def test_submit_wrong_guess():
+    # arrange
+    level = 4
+    game = Game(level)
+    game.answer = "1234"
+    guess = Guess("1111")
+    initial_lives = game.lives
+
+    # act
+    game.submit_user_guess(guess)
+
+    # assert
+    assert any(guess.guess_value == "1111" for guess in game.previous_guesses)
+    assert game.lives == initial_lives - 1
+    assert game.game_status == "In Progress"
+
+
+def test_judge_guess_all_correct():
+    # arrange
+    level = 4
+    game = Game(level)
+    game.answer = "1234"
+    guess = Guess("1234")
+
+    # act
+    guess.judge_guess(game.answer)
+
+    # assert
+    assert guess.correct_number == 4
+    assert guess.correct_location == 4
+    assert isinstance(guess.correct_number, int)
+    assert isinstance(guess.correct_location, int)
+    assert guess.correct_number is not None
+    assert guess.correct_location is not None
+
+
+def test_judge_guess_not_all_correct():
+    # arrange
+    level = 4
+    game = Game(level)
+    game.answer = "1234"
+    guess = Guess("0000")
+
+    # act
+    guess.judge_guess(game.answer)
+
+    # assert
+    assert guess.correct_number == 0
+    assert guess.correct_location == 0
+    assert isinstance(guess.correct_number, int)
+    assert isinstance(guess.correct_location, int)
+    assert guess.correct_number is not None
+    assert guess.correct_location is not None
+
+
+def test_judge_guess_partially_correct():
+    # arrange
+    level = 4
+    game = Game(level)
+    game.answer = "1234"
+    guess = Guess("7130")
+
+    # act
+    guess.judge_guess(game.answer)
+
+    # assert
+    assert guess.correct_number == 2
+    assert guess.correct_location == 1
+    assert isinstance(guess.correct_number, int)
+    assert isinstance(guess.correct_location, int)
+    assert guess.correct_number is not None
+    assert guess.correct_location is not None
+
+
+def test_judge_guess_with_repeated_digits_only():
+    # arrange
+    level = 4
+    game = Game(level)
+    game.answer = "1135"
+    guess = Guess("2211")
+
+    # act
+    guess.judge_guess(game.answer)
+
+    # assert
+    assert guess.correct_number == 2
+    assert guess.correct_location == 0
+    assert isinstance(guess.correct_number, int)
+    assert isinstance(guess.correct_location, int)
+    assert guess.correct_number is not None
+    assert guess.correct_location is not None
+
+
+def test_judge_guess_with_repeated_digits_only_and_correct_location():
+    # arrange
+    level = 4
+    game = Game(level)
+    game.answer = "1135"
+    guess = Guess("1122")
+
+    # act
+    guess.judge_guess(game.answer)
+
+    # assert
+    assert guess.correct_number == 2
+    assert guess.correct_location == 2
+    assert isinstance(guess.correct_number, int)
+    assert isinstance(guess.correct_location, int)
+    assert guess.correct_number is not None
+    assert guess.correct_location is not None
+
+
+def test_judge_guess_correct_digits_wrong_location():
+    # arrange
+    level = 4
+    game = Game(level)
+    game.answer = "1234"
+    guess = Guess("4321")
+
+    # act
+    guess.judge_guess(game.answer)
+
+    # assert
+    assert guess.correct_number == 4
+    assert guess.correct_location == 0
+    assert isinstance(guess.correct_number, int)
+    assert isinstance(guess.correct_location, int)
+    assert guess.correct_number is not None
+    assert guess.correct_location is not None
+
+
+def test_game_is_won():
+    # act
+    level = 4
+    game = Game(level)
+    game.answer = "1234"
+    guess = Guess("1234")
+
+    # arrange
+    game.submit_user_guess(guess)
+
+    # assert
+    assert game.game_status == "Won"
+
+
+def test_game_is_loss():
+    # act
+    level = 4
+    game = Game(level)
+    game.answer = "1234"
+    guess = Guess("7130")
+    game.lives = 1
+
+    # arrange
+    game.submit_user_guess(guess)
+
+    # assert
+    assert game.game_status == "Loss"
+    assert game.lives == 0
+
+# would need to test get valid guess
+# def test_submit_empty_guess():
+#     # arrange
+#     level = 4
+#     game = Game(level)
+#     game.answer = "1234"
+#     guess = Guess("")
+#     initial_lives = game.lives
+
+#     # act
+#     game.submit_user_guess(guess)
+
+#     # assert
+#     assert game.lives == initial_lives  # Lives shouldn't decrement
+#     assert not any(g.guess_value == "" for g in game.previous_guesses)
+
+# would need to test get valid guess
+# def test_submit_non_numeric_guess():
+    # # arrange
+    # level = 4
+    # game = Game(level)
+    # game.answer = "1234"
+    # guess = Guess("abcd")  # Non-numeric guess
+    # initial_lives = game.lives
+
+    # # act
+    # game.submit_user_guess(guess)
+
+    # # assert
+    # assert game.lives == initial_lives  # Lives shouldn't decrement
+    # assert not any(g.guess_value == "abcd" for g in game.previous_guesses)
+
+
+# def test_submit_repeated_guess():
+#     # arrange
+#     level = 4
+#     game = Game(level)
+#     game.answer = "1234"
+#     guess = Guess("1111")
+#     game.submit_user_guess(guess)
+#     initial_lives = game.lives
+
+#     # act
+#     game.submit_user_guess(guess)
+
+#     # assert
+#     assert game.lives == initial_lives  # Lives shouldn't decrement
+#     assert len(game.previous_guesses) == 1  # Guess shouldn't be added again
---
+++ view/console_view.py
@@ -0,0 +1,87 @@
+from art import game_logo, win_logo, lost_logo
+
+
+class View:
+
+    def display_instructions(self):
+        print(game_logo)
+        print("Welcome to Mastermind! \n")
+        print("Play against the computer and break the code to win. \n")
+        print("A random number from the digits 0-9 will be generated based on your chosen level. \n")
+        print("You will have 10 attempts to guess the number. \n")
+        print("Hints will be provided to help you along the way. \n")
+        print("Good luck! \n")
+
+    # Maybe move this function to the view & then call in play game method of controller
+    def get_user_difficulty_level(self):
+        levels = {
+            "easy": 4,
+            "medium": 6,
+            "hard": 8
+        }
+
+        while True:
+            level = input(
+                f"Please select a level to play (easy: {levels["easy"]} digits, medium: {levels["medium"]} digits, hard: {levels["hard"]} digits): ").lower()
+            if level not in levels:
+                print("Error: Please enter a valid level (easy, medium, hard).")
+            else:
+                return levels[level]
+
+    def display_lives_remaining(self, lives):
+        # lives will get passed in from the controller
+        print(f"You have {lives} attempts remaining. \n")
+
+    def get_valid_guess(self, level, previous_guesses):
+        # control flow concept - move to controller? / knows a little bit about validation & what constitutes a valid guess
+        # controller passes guess into game.is_valid_guess method - a validation method
+        # Maybe okay in view because it is basic validation?
+
+        # maybe move validation to game or guess model????
+        while True:
+            # User input - asks user to make a guess - view
+            guess = input(
+                f"Make a guess: \n"
+            )
+            if not guess.isnumeric():
+                # User feedback - checks for numeric value - view
+                print("Please enter a number with a numerical value. \n")
+            elif len(guess) != level:
+                # User feedback - checks for correct length - view
+                print(f"Please enter a number with {level} digits. \n")
+            elif guess in [guess.guess_value for guess in previous_guesses]:
+                # make this a view function to give feedback & get a valid new guess
+                print(f"You have already guessed {
+                      guess}. Please try again. \n")
+            else:
+                return guess
+
+    def display_guess_feedback(self, correct_number, correct_location):
+        # correct_number & correct_location get passed in from the controller
+        if correct_number == 0 and correct_location == 0:
+            print("All incorrect \n")
+        else:
+            print(f"{correct_number} correct number and {
+                  correct_location} correct location \n")
+
+    def display_game_over_feedback(self, answer, game_status):
+        # displays the feedback when a game is over -> won or loss
+        # game status & answer will get passed in from the controller
+        if game_status == "Won":
+            print(win_logo)
+            print(f"Congrats, you guessed the correct answer! \n")
+        else:
+            print(lost_logo)
+            print(f"Game over! The correct answer was: {answer} \n")
+
+    def get_restart_game_input(self):
+        while True:
+            user_input = input(
+                "Would you like to play again? (Enter 'yes' or 'no'): ").lower()
+            if user_input != "yes" and user_input != "no":
+                print("Please enter a valid response. \n")
+            elif user_input == "no":
+                print("Thanks for playing!")
+                return user_input
+            else:
+                return user_input
